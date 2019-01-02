b4eaf3ca54728ca454f890f8998b87d53d7d6f04
michael kalango <michaelkalango@gmail.com>
2019-01-02 21:35:52 +0100

    change password content

--- netsurf/lib/pages/change_password.dart
+++ netsurf/lib/pages/change_password.dart
@@ -16,7 +16,90 @@ class _ChangeScreenState extends State<ChangeScreen> {
         centerTitle: true,
       ),
       body: Center(
-        child: Text('Change Password'),
+        child: new SingleChildScrollView(
+            child: new Container(
+          padding: EdgeInsets.only(
+            left: 10.0,
+            right: 10.0,
+          ),
+          child: new SizedBox(
+            height: 680.0,
+            width: double.infinity,
+            child: new Card(
+              child: new Column(
+                children: <Widget>[
+                  new Padding(
+                    padding: const EdgeInsets.only(top: 10.0, bottom: 15.0),
+                    child: Icon(
+                      Icons.lock,
+                      color: Colors.greenAccent,
+                      size: 120.0,
+                    ),
+                  ),
+                  new Text(
+                    "Please fill in the form below \n to change your password",
+                    style: new TextStyle(fontWeight: FontWeight.w300),
+                  ),
+                  new Padding(
+                    padding: const EdgeInsets.only(
+                        left: 40.0, right: 40.0, top: 40.0, bottom: 10.0),
+                    child: new TextField(
+                      decoration: InputDecoration(
+                          prefixIcon: Icon(Icons.person_outline),
+                          hintText: 'john.doe'),
+                    ),
+                  ),
+                  new Padding(
+                    padding: const EdgeInsets.only(
+                        left: 40.0, right: 40.0, top: 10.0, bottom: 10.0),
+                    child: new TextField(
+                      decoration: InputDecoration(
+                          prefixIcon: Icon(Icons.lock_open),
+                          hintText: 'old password'),
+                    ),
+                  ),
+                  new Padding(
+                    padding: const EdgeInsets.only(
+                        left: 40.0, right: 40.0, top: 10.0, bottom: 10.0),
+                    child: new TextField(
+                      decoration: InputDecoration(
+                          prefixIcon: Icon(Icons.lock_open),
+                          hintText: 'new password'),
+                    ),
+                  ),
+                  new Padding(
+                    padding: const EdgeInsets.only(
+                        left: 40.0, right: 40.0, top: 10.0, bottom: 10.0),
+                    child: new TextField(
+                      decoration: InputDecoration(
+                          prefixIcon: Icon(Icons.lock_open),
+                          hintText: 'confirm new password'),
+                    ),
+                  ),
+                  new Padding(
+                    padding: const EdgeInsets.only(
+                        left: 40.0, right: 40.0, top: 10.0, bottom: 10.0),
+                    child: new TextField(
+                      decoration: InputDecoration(
+                          prefixIcon: Icon(Icons.lock_open),
+                          hintText: 'secret pin'),
+                    ),
+                  ),
+                  new Container(
+                    padding: EdgeInsets.only(top: 20.0, bottom: 20.0),
+                    child: new Center(
+                        child: new RaisedButton(
+                      child: const Text("submit"),
+                      elevation: 4.0,
+                      onPressed: () => null,
+                      splashColor: Colors.green
+                    )),
+                  )
+                ],
+              ),
+            ),
+          ),
+        )),
       ),
     );
   }
